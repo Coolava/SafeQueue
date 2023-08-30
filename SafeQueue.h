f9b89763f2ebe608580caadba9e230d8e708e243
YS Ryu <45839935+Coolava@users.noreply.github.com>
2023-08-30 15:32:11 +0900

    Update priority queue

--- SafeQueue.h
+++ SafeQueue.h
@@ -259,7 +259,7 @@ public:
         std::swap(q, emptyQueue);
     }
 
-	/*Queue¸¦ ÆÄ±«ÇÏÁö ¾Ê°í ·çÇÁ¸¦ Å»Ãâ ÇÒ ¶§ »ç¿ëÇÑ´Ù.*/
+	/*Queueë¥¼ íŒŒê´´í•˜ì§€ ì•Šê³  ë£¨í”„ë¥¼ íƒˆì¶œ í•  ë•Œ ì‚¬ìš©í•œë‹¤.*/
 	void escape() {
 		bEscape = true;
 		c.notify_one();
@@ -271,3 +271,183 @@ public:
     }
 };
 
+
+template<class T, class _Container = std::vector<T>, class _Pr = std::less<typename _Container::value_type>>
+class SafePriorityQueue
+{
+
+private:
+    //std::queue<T> q;
+    std::priority_queue<T, _Container, _Pr> q;
+    std::mutex m;
+    std::condition_variable c;
+    bool mIsLoop = true;
+    bool bEscape;
+    size_t limit = 50000;
+
+public:
+    SafePriorityQueue()
+        : q()
+        , m()
+        , c()
+        , mIsLoop(true)
+        , bEscape(false)
+    {}
+    ~SafePriorityQueue(void) {};
+
+    SafePriorityQueue(SafePriorityQueue<T, _Container, _Pr>&& other)
+        :q(std::move(other.q))
+    {
+    }
+
+    auto contatainer() { return q; }
+    /*Default : 4096*/
+    void setLimit(size_t _limit)
+    {
+        limit = _limit;
+    }
+
+    void enqueue(T&& t)
+    {
+
+        std::unique_lock<std::mutex> lock(m);
+        q.push(std::move(t));
+
+        /*for circular*/
+        if ((q.size() > limit)
+            && (limit > 0))
+        {
+            //q.front();
+            q.pop();
+        }
+
+        c.notify_one();
+    }
+
+
+    void enqueue(const T& t)
+    {
+
+        std::unique_lock<std::mutex> lock(m);
+        q.push(t);
+
+        /*for circular*/
+        if ((q.size() > limit)
+            && (limit > 0))
+        {
+            //q.front();
+            q.pop();
+        }
+
+        c.notify_one();
+    }
+
+
+
+    void enqueue(T* t, size_t n)
+    {
+        std::unique_lock<std::mutex> lock(m);
+
+        for (size_t i = 0; i < n; i++)
+        {
+            q.push(t[i]);
+        }
+
+        /*for circular*/
+        if ((q.size() > limit)
+            && (limit > 0))
+        {
+            //q.front();
+            q.pop();
+        }
+
+        c.notify_one();
+
+    }
+
+    // Get the "front"-element.
+    // If the queue is empty, wait till a element is avaiable.
+    T dequeue(void)
+    {
+        T val = T();
+        std::unique_lock<std::mutex> lock(m);
+        while ((mIsLoop) && (q.empty()) &&
+            bEscape == false)
+        {
+            // release lock as long as the wait and reaquire it afterwards.
+            // c.wait_for(lock,std::chrono::milliseconds(1));
+            c.wait(lock);
+        }
+        if (bEscape == false && mIsLoop == true)
+        {
+            val = q.top();
+            q.pop();
+        }
+        else
+        {
+            bEscape = false;
+        }
+
+        return val;
+    }
+
+    T dequeue_timeout(const std::chrono::milliseconds time, std::cv_status& status)
+    {
+        T val = T();
+        std::unique_lock<std::mutex> lock(m);
+        while ((mIsLoop) && (q.empty()) &&
+            bEscape == false)
+        {
+            // release lock as long as the wait and reaquire it afterwards.
+            // c.wait_for(lock,std::chrono::milliseconds(1));
+            status = c.wait_for(lock, time);
+
+            if (status == std::cv_status::timeout)
+            {
+                return T();
+            }
+        }
+        if (bEscape == false && mIsLoop == true)
+        {
+            val = q.top();
+            q.pop();
+        }
+        else
+        {
+            bEscape = false;
+        }
+
+        return val;
+    }
+
+    size_t size()
+    {
+        return q.size();
+    }
+
+    void swap(std::queue<T> Queue) {
+        q.swap(Queue);
+
+    }
+
+    void destory() {
+        mIsLoop = false;
+        c.notify_one();
+    }
+
+    void clear() {
+        std::unique_lock<std::mutex> lock(m);
+        std::queue<T> emptyQueue;
+        std::swap(q, emptyQueue);
+    }
+
+    void escape() {
+        bEscape = true;
+        c.notify_one();
+    }
+
+    bool isAvailable()
+    {
+        return mIsLoop;
+    }
+};
